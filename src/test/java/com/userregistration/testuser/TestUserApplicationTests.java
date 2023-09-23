a077f7a585fa436456f0da40683709120e59df39
Marwen <naguezmarouen@gmail.com>
2023-09-23 13:59:41 +0200

    [USER-1] : init

---
+++ src/test/java/com/userregistration/testuser/TestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userregistration.testuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
