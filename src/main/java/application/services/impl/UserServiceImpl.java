634f5a312c764086263d15ca54a0e0a99cc6650b
Marwen <naguezmarouen@gmail.com>
2023-10-05 10:52:30 +0200

    [USER-7] : modification services

--- src/main/java/application/services/impl/UserServiceImpl.java
+++ src/main/java/application/services/impl/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package application.services.impl;
 
+import application.controllers.UserController;
 import application.entities.Country;
 import application.entities.User;
+import application.exceptions.NotValidDataException;
+import application.exceptions.UserNotAuthorizedException;
 import application.repositories.CountryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import application.repositories.UserRepository;
 import application.services.UserService;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Service
@@ -16,6 +22,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final CountryRepository countryRepository;
 
+    private static final Logger logger = LogManager.getLogger(UserController.class);
 
     public UserServiceImpl(UserRepository userRepository, CountryRepository countryRepository) {
         this.userRepository = userRepository;
@@ -27,14 +34,40 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Long saveUser(User user) {
+    public Long saveUser(User user) throws UserNotAuthorizedException, NotValidDataException {
         Country country = countryRepository.findById(user.getCountry().getId()).orElse(null);
         user.setCountry(country);
+        checkUser(user);
         return userRepository.save(user).getUserId();
     }
 
     @Override
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    private void checkUser(User user)  throws UserNotAuthorizedException, NotValidDataException {
+        if (user.getCountry() == null || user.getUserName() == null || user.getBirthDateUser() == null){
+            logger.error("Please fill in all required fields!");
+            throw new NotValidDataException("Please check the input fields!");
+        }
+        if (user.getCountry().getId() == 1 && (calculAge(user.getBirthDateUser()))>= 18){
+            logger.info("User "+user.getUserName()+" successfully created");
+        }else {
+            logger.error("The user creation failed!");
+            throw new UserNotAuthorizedException("Only adult French residents are allowed to create an account!");
+        }
+
+    }
+
+    private int calculAge(LocalDate birthDate){
+        // Date actuelle
+        LocalDate dateActuelle = LocalDate.now();
+
+        // Calcul de la période entre la date de naissance et la date actuelle
+        Period difference = Period.between(birthDate, dateActuelle);
+
+        // Récupération de l'âge
+        return difference.getYears();
     }
 }
--- src/test/java/application/services/impl/UserServiceImplTest.java
+++ src/test/java/application/services/impl/UserServiceImplTest.java
@@ -2,6 +2,9 @@ package application.services.impl;
 
 import application.entities.Country;
 import application.entities.User;
+import application.exceptions.NotValidDataException;
+import application.exceptions.UserNotAuthorizedException;
+import application.exceptions.UserNotFoundException;
 import application.repositories.CountryRepository;
 import application.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -46,14 +51,37 @@ public class UserServiceImplTest {
         assertEquals(mockUser, returnedUser);
     }
 
+
     @Test
-    void how_to_save_user() {
+    void how_to_get_all_user() throws UserNotFoundException {
+        //GIVEN
+        User mockUser1 = new User();
+        mockUser1.setUserId(1L);
+        mockUser1.setUserName("Marwen Naguez");
+        User mockUser2 = new User();
+        mockUser2.setUserId(1L);
+        mockUser2.setUserName("Laurent");
+        List<User> users = List.of(mockUser1,mockUser2);
+
+        when(userService.getUsers()).thenReturn(users);
+
+        //WHEN
+        List<User> returnedUsers = userService.getUsers();
+
+        //THEN
+        assertEquals(users, returnedUsers);
+    }
+
+
+    @Test
+    void how_to_save_user() throws UserNotAuthorizedException, NotValidDataException {
         // GIVEN
         User newUser = new User();
         newUser.setUserName("Marwen Naguez");
         Country country = new Country();
         country.setId(1L);
         newUser.setCountry(country);
+        newUser.setBirthDateUser(LocalDate.of(1990,11,23));
 
         when(countryRepository.findById(1L)).thenReturn(Optional.of(country));
         when(userRepository.save(newUser)).thenReturn(newUser);
@@ -65,5 +93,61 @@ public class UserServiceImplTest {
         assertEquals(newUser.getUserId(), returnedUser);
 
         verify(userRepository, times(1)).save(newUser);
+    }
+
+    @Test
+    void how_to_save_user_not_authorized_age() {
+        //GIVEN
+        User newUser = new User();
+        newUser.setUserName("Marwen Naguez");
+        Country country = new Country();
+        country.setId(1L);
+        newUser.setCountry(country);
+        newUser.setBirthDateUser(LocalDate.of(2080, 1, 1));
+
+        //WHEN
+        try {
+            userService.saveUser(newUser);
+        } catch (UserNotAuthorizedException | NotValidDataException e) {
+            //THEN
+            assertEquals("Please check the input fields!", e.getMessage());
+        }
+    }
+    @Test
+    void how_to_save_user_not_authorized_country() {
+        //GIVEN
+        User newUser = new User();
+        newUser.setUserName("Marwen Naguez");
+        Country country = new Country();
+        country.setId(2L);
+        newUser.setCountry(country);
+        newUser.setBirthDateUser(LocalDate.of(1990, 1, 1));
+        when(countryRepository.findById(2L)).thenReturn(Optional.of(country));
+
+        //WHEN
+        try {
+            userService.saveUser(newUser);
+        } catch (UserNotAuthorizedException | NotValidDataException e) {
+            //THEN
+            assertEquals("Only adult French residents are allowed to create an account!", e.getMessage());
+        }
+    }
+
+    @Test
+    void how_to_save_user_with_empty_data() {
+        //GIVEN
+        User newUser = new User();
+        Country country = new Country();
+        country.setId(1L);
+        newUser.setCountry(country);
+        newUser.setBirthDateUser(LocalDate.of(1990, 1, 1));
+
+        //WHEN
+        try {
+            userService.saveUser(newUser);
+        } catch (UserNotAuthorizedException | NotValidDataException e) {
+            //THEN
+            assertEquals("Please check the input fields!", e.getMessage());
+        }
     }
 }
